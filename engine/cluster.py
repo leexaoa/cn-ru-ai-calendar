751d1aaa4efc05dc2a86435633976498385a05ef
LEE <leexaoa@gmail.com>
2026-05-11 13:18:14 +0300

    Update cluster.py

--- engine/cluster.py
+++ engine/cluster.py
@@ -1,1 +1,29 @@
+from datetime import datetime, timedelta
 
+def to_date(d):
+    return datetime.strptime(d, "%Y-%m-%d").date()
+
+
+def cluster_dates(dates):
+    """
+    把连续日期合并成区间
+    """
+    if not dates:
+        return []
+
+    dates = sorted([to_date(d) for d in dates])
+
+    clusters = []
+    start = dates[0]
+    prev = dates[0]
+
+    for d in dates[1:]:
+        if (d - prev).days == 1:
+            prev = d
+        else:
+            clusters.append((start, prev))
+            start = d
+            prev = d
+
+    clusters.append((start, prev))
+    return clusters
